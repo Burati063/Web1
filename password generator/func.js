e17769c9351856accdfcdcc22d3a0c9ad6152ad9
biruk kassahun <birukkass4@gmail.com>
2025-03-02 22:27:15 +0300

    fix the js part

--- password generator/func.js
+++ password generator/func.js
@@ -1,17 +1,18 @@
 var password= document.getElementById("password");
 function genPassword(){
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&* ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-    var passLength = "";
-    for(var i=0; i < passLength;i++){
-        var randomNumber = Math.floor(Math.random()* chars.length);
+    var chars = "0123456789abcdefghigklmnopqrswxyz!@#$%^&*()ABCDEFGHIGKLMNOPQRSWXYZ";
+    var passLength = 10;
+    var password = "";
+    for (var i = 0; i < passLength; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1);
 
     }
- document.getElementById("password").value = password;
-
+    document.getElementById("password").value = password;
 }
+
 function copyPassword(){
     var copyText = document.getElementById("password");
-    copyText.Select();
+    copyText.select();
     document.execCommand("copy");
 }
